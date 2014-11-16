ae7857bcc988856a7b6dd6fe6e24049f70ed1218
Tsahi <t.tsahi@gmail.com>
2014-11-16 13:44:52 +0200

    init commit

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name presetsApp
+ * @description
+ * # presetsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('presetsApp', [
+    'ngAnimate',
+    'ngSanitize'
+  ]);
