cda33da871812d0f53d6135c827005df73228d68
Tsahi <t.tsahi@gmail.com>
2014-11-17 14:04:26 +0200

    Add core annapolis dependencies.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,15 +1,23 @@
-'use strict';
+(function(){
 
-/**
- * @ngdoc overview
- * @name presetsApp
- * @description
- * # presetsApp
- *
- * Main module of the application.
- */
-angular
-  .module('presetsApp', [
-    'ngAnimate',
-    'ngSanitize'
-  ]);
+    'use strict';
+
+    /**
+     * @ngdoc overview
+     * @name presetsApp
+     * @description
+     * # presetsApp
+     *
+     * Main module of the application.
+     */
+    angular
+        .module('presetsApp', [
+            'ngAnimate',
+            'ngSanitize',
+            'ui.router',
+            'restangular',
+            'annapolis-data',
+            'annapolis-map'
+        ]);
+
+}());
---
+++ app/scripts/config.js
@@ -0,0 +1,15 @@
+/**
+ * Created by tzachit on 16/11/14.
+ */
+
+(function(app){
+
+    'use strict';
+
+    app.config(['$stateProvider', '$urlRouterProvider',
+        function($stateProvider, $urlRouterProvider){
+
+        }
+    ]);
+
+}(angular.module('presetsApp')));
